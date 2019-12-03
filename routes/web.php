18c32ab5b2cd2cd3bafc809f5c4ad85134d08292
rajib chisim <rajibchisim@gmail.com>
2019-12-03 16:57:01 +0600

    auth implementation

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,6 @@ Route::get('about', 'PagesController@about');
 Route::get('service', 'PagesController@service');
 
 Route::resource('admin', 'ServiceController');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
